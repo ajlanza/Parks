787bcb6518eb29351998f6d4a2a013120510a94a
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-04-09 09:52:59 -0400

    Working
    Code works as long as user inputs information correctly. Additional code needed to improve layout and error checking.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+// 'use strict';
 
 function watch() {
     $('form').submit(event => {
@@ -8,7 +6,32 @@ function watch() {
         const state = $('#searchState').val();
         const maxResults = $('#searchMaxResults').val();
         console.log(state, maxResults);
+        getParks(state, maxResults);
     });
 }
+
+function getParks(st, max) {
+  const query = `?stateCode=${st}&limit=${max}`;
+  const url = `https://developer.nps.gov/api/v1/parks${query}&api_key=7fN4nAIssMTPpz0i9OewrBGvD38RiGSCyNQtcpAr`;
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson, max))
+    .catch(error => alert("It broke!"));
+    }
+
+function displayResults(responseJson, max) {
+    console.log("displaying results");
+    document.getElementById('results').innerHTML = "";
+    for (let i = 0; i < max; i++){
+      let name = responseJson.data[i].fullName;
+      let description = responseJson.data[i].description;
+      let parkUrl = responseJson.data[i].url;
+      console.log(name, parkUrl);
+      $("ul#results").append(`<li>${name}: <a href="${parkUrl}">${parkUrl}</a></li><br>
+      <li>${description}</li><br><br>`);
+    }
+    $(".hidden").removeClass("hidden");
+}
+
 
 $(watch);
